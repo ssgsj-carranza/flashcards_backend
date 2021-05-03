6b0c3d348b5698bc08f40dd13a36af13b8cfd6a6
unknown <ssgsj.carranza@gmail.com>
2021-05-03 12:57:20 -0500

    added a flashcard list class with get and post methods

--- flashcards_backend/flashcard/views.py
+++ flashcards_backend/flashcard/views.py
@@ -6,4 +6,22 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
 
+
 # Create your views here.
+
+class FlashcardList(APIView):
+
+    def get(self, request):
+        flashcard = Flashcard.objects.all()
+        serializer = FlashcardSerializer(flashcard, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = FlashcardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
