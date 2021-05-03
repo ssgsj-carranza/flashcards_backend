653f659cdb70f35ca86a5acc1b35c008fb274669
unknown <ssgsj.carranza@gmail.com>
2021-05-03 13:03:35 -0500

    made a flashcard detail class with methods to get delete and put

--- flashcards_backend/flashcard/views.py
+++ flashcards_backend/flashcard/views.py
@@ -24,4 +24,31 @@ class FlashcardList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class FlashcardDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Flashcard.objects.get(pk=pk)
+        except Flashcard.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        flashcard = self.get_object(pk)
+        serializer = FlashcardSerializer(flashcard)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        flashcard = self.get_object(pk)
+        serializer = FlashcardSerializer(flashcard, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        flashcard = self.get_object(pk)
+        flashcard.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
